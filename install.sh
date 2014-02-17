886b5e2c271ee126616bd639e2a21ce111843af7
Brandon Myers <bdmyers@cs.washington.edu>
2014-02-17 14:08:39 -0800

    install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+ln -s spark/conf/spark-env.sh $SPARK_HOME/conf/spark-env.sh
+ln -s shark/conf/shark-env.sh $SHARK_HOME/conf/shark-env.sh
+ln -s hive/conf/hive-site.xml $HIVE_HOME/conf/hive-site.xml
+ln -s hive/conf/hive-env.sh $HIVE_HOME/conf/hive-env.sh
