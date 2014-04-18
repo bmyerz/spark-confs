53948ad6a03d5da66f3f580151bb07a08f3b8fa2
Brandon Myers <bdmyers@cs.washington.edu>
2014-04-18 16:06:47 -0700

    build just the desired example file and put it into the jar

---
+++ spark/examples/build-class.sh
@@ -0,0 +1,15 @@
+filebase=$1
+#SCALA_HOME=~/escience/spark/scala-2.10.4
+PKG=org/apache/spark/examples
+
+mkdir -p target/scala-2.9.3/classes
+pushd target/scala-2.9.3/classes
+CLASSPATH=`../../../../bin/compute-classpath.sh`
+#$SCALA_HOME/bin/scalac -classpath $CLASSPATH ../../../src/main/scala/$GRAPHX_PKG/$filebase.scala
+scalac -classpath $CLASSPATH ../../../src/main/scala/$PKG/$filebase.scala
+popd
+
+# update the jar file
+pushd src/main/scala
+jar uf ../../spark-examples_2.9.3-0.8.0-incubating.jar $PKG/$filebase*.class
+popd
